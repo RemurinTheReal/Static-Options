ced1598a20af808ef747ee00f335443afcc7d316
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-08-31 17:03:02 -0400

    Fix Linux

--- common/src/main/java/com/remurinthereal/staticoptions/client/StaticOptionsClient.java
+++ common/src/main/java/com/remurinthereal/staticoptions/client/StaticOptionsClient.java
@@ -19,8 +19,9 @@ public final class StaticOptionsClient {
             case OSX:
                 STATIC_OPTIONS_PATH = System.getProperty("user.home") + "\\Library\\Application Support\\minecraft";
                 break;
+            case SOLARIS:
             case LINUX:
-                STATIC_OPTIONS_PATH = System.getProperty("user.home") + "\\.config\\.minecraft";
+                STATIC_OPTIONS_PATH = System.getProperty("user.home") + "\\.minecraft";
                 break;
             default:
                 STATIC_OPTIONS_PATH = null;
